7dc2b1188c40b06852698f88beec027f7ede1127
Laz627 <169095963+Laz627@users.noreply.github.com>
2024-07-07 23:18:56 -0400

    Update main.py

--- main.py
+++ main.py
@@ -38,7 +38,7 @@ def process_file(uploaded_file, designated_domains):
 
     # Sort by estimated traffic and then by search volume if estimated traffic is zero
     domain_traffic['Sort Order'] = np.where(domain_traffic['Total Estimated Traffic'] == 0, domain_traffic['Total Search Volume'], domain_traffic['Total Estimated Traffic'])
-    domain_traffic = domain_traffic.sort_values(by=['Sort Order', 'Total Search Volume'], ascending=[False, False]).reset_index(drop=True)
+    domain_traffic = domain_traffic.sort_values(by=['Total Estimated Traffic', 'Total Search Volume'], ascending=[False, False]).reset_index(drop=True)
 
     # Add ranking column for full list
     domain_traffic['Rank'] = domain_traffic.index + 1
